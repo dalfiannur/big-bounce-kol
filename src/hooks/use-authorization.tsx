4ee672f19e93ef470147f02f7b53079417dd294a
dalfiannur <dalfiannur@gmail.com>
2025-08-02 00:31:09 +0700

    Add admin-only access logic to `useAuthorization` hook

--- src/hooks/use-authorization.tsx
+++ src/hooks/use-authorization.tsx
@@ -1,13 +1,19 @@
 import {useEffect} from 'react'
 import {useRouter} from 'next/navigation'
+import {useUser} from '@/hooks/use-user'
 
-export const useAuthorization = () => {
+export const useAuthorization = (isAdmin: boolean) => {
 	const router = useRouter()
+	const user = useUser()
 	
 	useEffect(() => {
 		const accessToken = localStorage.getItem('access_token')
 		if (!accessToken) {
 			router.push('/login')
 		}
-	}, [])
+		
+		if (!isAdmin && user && user.role.name !== 'Administrator') {
+			router.push('/login')
+		}
+	}, [user, router, isAdmin])
 }
